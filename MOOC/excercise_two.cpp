b13e68cdccfdaabf0ad024ec09c3eb4c017305aa
shivanshsinghx365 <shivanshsinghx365@gmail.com>
2022-02-09 15:02:24 +0530

    lecture 1 part 3

---
+++ MOOC/excercise_two.cpp
@@ -0,0 +1,11 @@
+#include <simplecpp>
+int main(){
+    cout<<"a";
+    repeat(5){
+        cout<<"b";
+        repeat(2){
+            cout<<"c";
+        }
+        cout<<"d";
+    }
+}
--- MOOC/third.cpp
+++ MOOC/third.cpp
@@ -6,4 +6,13 @@
 //Arguments can be none, one or many
 //Syntax is used to refer the form or grammar that a language is supposed to follow
 //Programming languages have very strict syntax
-//
+//some amount of flexibility is allowed
+//Program will be executed by computer as well as read by people
+//human readable comments are placed to help readers understand about the program
+//comments are ignored by the computer
+//indentation help us understand which statements are a part of which functions 
+//indentation is also ignored during execution
+//control is at statement w :-> means that computer is currently executing statement w
+//control flow :-->  the order in which the commands get executed
+//variable:--> region of memory designated for storing some value you need
+//value of a variable can be changed thats why we can call it variable
