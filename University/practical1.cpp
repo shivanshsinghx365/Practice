baef7233f389bb36d812cbd908175ff948af8d27
shivanshsinghx365 <shivanshsinghx365@gmail.com>
2022-02-14 10:58:35 +0530

    a to z, 10 to 1

--- University/practical1.cpp
+++ University/practical1.cpp
@@ -1,6 +1,16 @@
+//WAP to find print A-z using loop
+//WAP to print 10-1 using loop
+
 #include <iostream>
 using namespace std;
 int main(){
-     
+    for (char ch='A';ch<='Z';ch++){
+        cout<<ch<<' ';
+    }
+    cout<<endl;
+    for (int i=10;i>=1;i--){
+        cout<<i<<' ';
+    }
+    cout<<endl;
     return 0;
 }
