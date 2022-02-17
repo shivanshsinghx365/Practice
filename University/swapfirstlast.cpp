818eaa197d4b69d3d37c2c6b8c2e6b78f3ab598c
shivanshsinghx365 <shivanshsinghx365@gmail.com>
2022-02-17 19:12:52 +0530

    WAP to swap first and last of any number

--- University/swapfirstlast.cpp
+++ University/swapfirstlast.cpp
@@ -7,5 +7,25 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int first,remain,k,last,m,num,z;
+    int i=0;
+    cout<<"Enter : ";
+    cin>>m;
+    num=m;
+    while (m>0){
+        i=i+1;
+        m=(int)(m/10);
+    }
+    last=num%10;
+    k=1;
+    while(i>1){
+        k*=10;
+        i--;
+    }
+    first=num/k;
+    remain=(num%k)-last;
+    z=remain+first;
+    z+=(last*k);
+    cout<<"Output :"<<z<<endl;
 }
+//cin and cout are objects
