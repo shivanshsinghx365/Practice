d0d31a87c1c67fb4c57e1a058e8c9071ae6eb485
shivanshsinghx365 <shivanshsinghx365@gmail.com>
2022-02-26 12:12:59 +0530

    divisible by 5 check
    multiple methods used

---
+++ Random/five.c
@@ -0,0 +1,44 @@
+//NOTE : USE ONLY ONE METHOD AT A TIME !
+
+#include <stdio.h>
+
+//Method 2 -- Recursion if-else
+void func(){                                            //2
+    int n;                                              //2
+    printf("Enter a number divisible by 5 : ");         //2
+    scanf("%d",&n);                                     //2
+    if (n%5!=0){                                        //2
+        func();                                         //2
+    }                                                   //2
+    else{                                               //2
+        printf("%d is divisible by 5\n",n);             //2
+    }                                                   //2
+}                                                       //2
+
+
+void main(){
+    //Method 1  -- do while
+    int n;                                              //1
+    do{                                                 //1
+        printf("Enter a number divisible by 5 : ");     //1
+        scanf("%d",&n);                                 //1
+    }                                                   //1
+    while(n%5!=0);                                      //1
+    printf("%d is divisible by 5\n",n);                 //1
+    //Method 2
+    func();                                             //2
+    //Method 3 -- while
+    int n=1;                                            //3
+    while(n%5!=0){                                      //3
+        printf("Enter a number divisible by 5 : ");     //3
+        scanf("%d",&n);                                 //3
+    }                                                   //3
+    printf("%d is divisible by 5\n",n);                 //3
+    //Method 4 -- For
+    int n;                                              //4                         
+    for(n=1;n%5!=0;n+=0){                               //4
+        printf("Enter a number divisible by 5 : ");     //4
+        scanf("%d",&n);                                 //4
+    }                                                   //4
+    printf("%d is divisible by 5\n",n);                 //4
+}
