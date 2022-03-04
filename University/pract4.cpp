0126848bfb4f7d7e2ce0da09fe37ccfbaa9f05ad
shivanshsinghx365 <shivanshsinghx365@gmail.com>
2022-03-04 19:29:18 +0530

    exp 3 start

---
+++ University/pract4.cpp
@@ -0,0 +1,9 @@
+/*Practical 3.4: WAP to create a class complex to represent complex numbers.
+The complex class should use a function to add two complex numbers which are passed as arguments.
+The function should return an object of type complex representing the sum of two complex numbers.*/
+#include <iostream>
+using namespace std;
+class complex{
+
+};
+int main(){}
