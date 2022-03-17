33cc801f09d6744223b028a8e33dfa829e624264
Shivansh Singh <shivanshsinghx365@gmail.com>
2022-03-17 17:04:02 +0530

    Update construct_destruct.cpp

--- C++/construct_destruct.cpp
+++ C++/construct_destruct.cpp
@@ -19,3 +19,15 @@ int main(){
 
 // O/p is    Today   Hii   Today Today  Exit  Exit    Bye   Exit 
 // reason->   n1    main    n2    n3    ~n3    ~n2   main   ~n3
+
+/*constructor doesn't have return type
+automatically called when object is made
+it provides memory in objects
+even when you don't create it's automatically called in backend
+
+destructor vacates the memory
+has tilda ~ and same name as class
+it can never have parameters
+works when objects scope end Or when main function ends
+
+construct  and destruct have reverse order like 123 construct and reverse is 321 Destruction*/
