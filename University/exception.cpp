92ac44769c00528c9ad5a6c50cef5b0a06e83703
shivanshsinghx365 <shivanshsinghx365@gmail.com>
2022-04-19 10:04:46 +0530

    exception

---
+++ University/exception.cpp
@@ -0,0 +1,24 @@
+//Exceptional handling deals with RUN-TIME EXCEPTIONS
+//try : tries the code and checks for errors
+//except : this code executes if no error occurred
+//throw : throws an exception
+#include <iostream>
+using namespace std;
+int main(){
+    int n,d;
+    cout<<"Enter numerator : ";
+    cin>>n;
+    cout<<"Enter denominator : ";
+    cin>>d;
+    try{
+        if (d==0){
+            throw d;
+        }
+        else{
+            cout<<"Result : "<<n/d<<endl;
+        }
+    }
+    catch(int d){
+        cout<<"Division by "<<d<<" Error"<<endl;
+    }
+}   
