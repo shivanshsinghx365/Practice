461ee2d6cf94bb0eec9f4e9bbc160ad3dad42c29
shivanshsinghx365 <shivanshsinghx365@gmail.com>
2022-05-12 12:16:26 +0530

    change

--- University/file_handling.cpp
+++ University/file_handling.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 int main(){
+    ofstream f;
+    f.open("hello_world.txt");
+    f<<"Hello from earth";
+    f.close();
+
+    ifstream fh;
+    fh.open("hello_world.txt");
     
+    fh.close();
     return 0;
 }
