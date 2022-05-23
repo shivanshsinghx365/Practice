ea5b205a6c69f8b0875194d5e6cfbc26614b7103
shivanshsinghx365 <shivanshsinghx365@gmail.com>
2022-05-23 17:25:28 +0530

    jump

--- Complete Interview Prep/2. Language Basics/C++/9. Jump Statement/return.cpp
+++ Complete Interview Prep/2. Language Basics/C++/9. Jump Statement/return.cpp
@@ -4,6 +4,7 @@ int main(){
     cout<<"Begin ";
     for (int i=0;i<10;i++){
         if (i==5)
+        //return is even stronger than break
         return 0; //when return executed the entire program ends not just a loop
         //so output would have been Begin 0 1 2 3 4
         //then program ends
